2e1e71480c7d000ab1d73f225ac65718aef8639e
Thomas Cokelaer <cokelaer@gmail.com>
2019-12-21 20:54:59 +0100

    update cookie cutter

--- hooks/pre_gen_projects.py
+++ hooks/pre_gen_projects.py
@@ -5,23 +5,19 @@ import sys
 MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
 
 module_name = '{{ cookiecutter.name }}'
+project_slug = '{{ cookiecutter.project_slug }}'
 
-print("hello")
+
+
+if project_slug != "sequana_" + module_name:
+
+    print("In the project_slug choice, just press enter. Do not overwrite the default value")
+    sys.exit(1)
+
 
 if not re.match(MODULE_REGEX, module_name):
     print('ERROR: %s is not a valid Python module name!' % module_name)
 
     # exits with status 1 to indicate failure
     sys.exit(1)
-else:
-    print("""Welcome to sequana_pipeline_template
-
-        You will be asked 3 questions.
-
-        - The first question: the name of your new pipeline (the prefix sequana_
-          will be added automatically)
-        - The second question: a short description
-        - The third question: a list of keywords separated by commas
-
-        """)
 
