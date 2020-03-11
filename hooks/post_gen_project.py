4a0c82fae4bd66ac96770ed2632b215dd185e410
Thomas Cokelaer <cokelaer@gmail.com>
2020-03-11 13:45:44 +0100

    better information on screen

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,6 +4,58 @@ import sys
 
 
 
-print("####################################")
 
+# THIS SHOULD REMAINS PURE PYTHON
+
+
+class Colors:
+    """
+
+    ::
+
+        color = Colors()
+        print(color.failed("msg"))
+
+    """
+    PURPLE = "\033[95m"
+    BLUE = "\033[94m"
+    GREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+    def failed(self, msg):
+        return self.FAIL + msg + self.ENDC
+
+    def bold(self, msg):
+        return self.BOLD + msg + self.ENDC
+
+    def purple(self, msg):
+        return self.PURPLE + msg + self.ENDC
+
+    def underlined(self, msg):
+        return self.UNDERLINE + msg + self.ENDC
+
+    def fail(self, msg):
+        return self.FAIL + msg + self.ENDC
+
+    def error(self, msg):
+        return self.FAIL + msg + self.ENDC
+
+    def warning(self, msg):
+        return self.WARNING + msg + self.ENDC
+
+    def green(self, msg):
+        return self.GREEN + msg + self.ENDC
+
+    def blue(self, msg):
+        return self.BLUE + msg + self.ENDC
+
+colors = Colors()
+
+print(colors.purple("Your pipeline layout is now ready in {{cookiecutter.name}}/ directory"))
+print(colors.purple("Thanks for using sequana_start_pipeline / sequana_pipeline_template cookiecutter"))
+print(colors.purple("Any suggestions welcome on https://github.com/sequana/sequana project"))
 
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -7,18 +7,68 @@ MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
 module_name = '{{ cookiecutter.name }}'
 project_slug = '{{ cookiecutter.project_slug }}'
 
-print("\n\nCreating the {} package".format(project_slug))
+
+
+# THIS SHOUL REMAINS PURE PYTHON
+
+
+class Colors:
+    """
+
+    ::
+
+        color = Colors()
+        print(color.failed("msg"))
+
+    """
+    PURPLE = "\033[95m"
+    BLUE = "\033[94m"
+    GREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+    def failed(self, msg):
+        return self.FAIL + msg + self.ENDC
+
+    def bold(self, msg):
+        return self.BOLD + msg + self.ENDC
+
+    def purple(self, msg):
+        return self.PURPLE + msg + self.ENDC
+
+    def underlined(self, msg):
+        return self.UNDERLINE + msg + self.ENDC
+
+    def fail(self, msg):
+        return self.FAIL + msg + self.ENDC
+
+    def error(self, msg):
+        return self.FAIL + msg + self.ENDC
+
+    def warning(self, msg):
+        return self.WARNING + msg + self.ENDC
+
+    def green(self, msg):
+        return self.GREEN + msg + self.ENDC
+
+    def blue(self, msg):
+        return self.BLUE + msg + self.ENDC
+
+
+colors = Colors()
+print(colors.purple("\n\nCreating the {} package.".format(project_slug)))
 
 if not re.match(MODULE_REGEX, module_name):
-    print("ERROR: '%s' is not a valid Python module name ! \n" % module_name)
+    print(colors.failed("ERROR: '%s' is not a valid Python module name ! \n" % module_name))
     # exits with status 1 to indicate failure
     sys.exit(1)
 else:
-    print("The name provided ({}) seems correct".format(module_name))
+    print(colors.purple("The name provided ({}) seems correct. Congratulations !".format(module_name)))
 
 
 if project_slug != "sequana_" + module_name:
-    print("In the project_slug choice, just press enter. Do not overwrite the default value")
+    print(colors.purple("In the project_slug choice, just press enter. Do not overwrite the default value"))
     sys.exit(1)
-
-
