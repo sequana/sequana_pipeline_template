f54a54d9215c79e3228adae84d4cd6c3baef6a09
Thomas Cokelaer <cokelaer@gmail.com>
2021-08-29 18:24:30 +0200

    Update cookie cutter (test)

--- {{cookiecutter.project_slug}}/test/test_main.py
+++ {{cookiecutter.project_slug}}/test/test_main.py
@@ -1,43 +1,23 @@
-import easydev
 import os
-import tempfile
 import subprocess
 import sys
-from sequana_pipetools.pipelines_common import get_pipeline_location as getpath
 
-sharedir = getpath('{{cookiecutter.name}}')
-
-
-def test_standalone_subprocess():
-    directory = tempfile.TemporaryDirectory()
-    cmd = """sequana_pipelines_{{cookiecutter.name}} --input-directory {} 
-            --working-directory {} --force""".format(sharedir, directory.name)
-    subprocess.call(cmd.split())
+import sequana_pipelines.{{cookiecutter.name}}.main as m
 
 
-def test_standalone_script():
-    directory = tempfile.TemporaryDirectory()
-    import sequana_pipelines.{{cookiecutter.name}}.main as m
-    sys.argv = ["test", "--input-directory", sharedir, 
-            "--working-directory", directory.name, "--force"]
+def test_standalone_subprocess(tmpdir):
+    input_dir = os.sep.join((test_dir, 'resources'))
+    cmd = ["test", "--input-directory", input_dir, "--working-directory", str(tmpdir), "--force"]
+    subprocess.call(cmd)
+
+
+def test_standalone_script(tmpdir):
+    input_dir = os.sep.join((test_dir, 'resources'))
+    sys.argv = ["test", "--input-directory", input_dir, "--working-directory", str(tmpdir), "--force"]
     m.main()
 
-def test_full():
-
-    with tempfile.TemporaryDirectory() as directory:
-        print(directory)
-        wk = directory
-
-        cmd = "sequana_pipelines_{{cookiecutter.name}} --input-directory {} "
-        cmd += "--working-directory {}  --force"
-        cmd = cmd.format(sharedir, wk)
-        subprocess.call(cmd.split())
-
-        stat = subprocess.call("sh {{cookiecutter.name}}.sh".split(), cwd=wk)
-
-        assert os.path.exists(wk + "/multi_summary.html")
 
 def test_version():
-    cmd = "sequana_pipelines_{{cookiecutter.name}} --version"
-    subprocess.call(cmd.split())
+    cmd = ["sequana_pipelines_{{cookiecutter.name}}, --version"]
+    subprocess.call(cmd)
 
